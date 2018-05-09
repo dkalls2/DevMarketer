39f24338a22d872bdaea4d6f754bb7a427b7d57f
Drew Kallstrom <drew.kallstrom@gmail.com>
2018-05-09 18:36:18 -0500

    minor changes to clean up blade files and straggling SASS

---
+++ resources/views/_includes/nav/main.blade.php
@@ -0,0 +1,60 @@
+<nav class="navbar has-shadow">
+    <div class="container">
+        <div class="navbar-brand" role="navigation" aria-label="main navigation">
+            <a class="navbar-item" href="{{route('home')}}">
+                <img src = "{{asset('images/devmarketer-logo.png')}}" alt="DevMarketer Logo" />
+                {{-- From the asset helper, we can reference anything in the public folder...we can just do the url from the public folder. --}}
+            </a>
+
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+
+        <div class="navbar-menu" role="navigation" aria-label="main navigation">
+            <div class="navbar-start">
+                <a href="#" class="navbar-item is-tab is-hidden-mobile m-l-10">Learn</a>
+                <a href="#" class="navbar-item is-tab is-hidden-mobile">Discuss</a>
+                <a href="#" class="navbar-item is-tab is-hidden-mobile">Share</a>
+            </div>
+
+            <div class="navbar-end">
+                @if (Auth::guest())
+                    <a href="{{route('login')}}" class="navbar-item is-tab">Login</a>
+                    <a href="{{route('register')}}" class="navbar-item is-tab">Join the Community</a>
+                @else
+                    <div class="navbar-item has-dropdown is-hoverable">
+                        <a class="navbar-item is-tab">
+                            <span>Hey Alex</span>
+                            <span class="icon"><i class="fa fa-caret-down"></i></span>
+                        </a>
+                    
+                        <div class="navbar-dropdown is-right">
+                            <a href="#" class="navbar-item">
+                                <span class="icon"><i class="fa fa-fw m-r-10 fa-user-circle-o"></i></span>
+                                Profile
+                            </a>
+                            <a href="#" class="navbar-item">
+                                <span class="icon"><i class="fa fa-fw m-r-10 fa-bell"></i></span>
+                                Notifications
+                            </a>
+                            <a href="#" class="navbar-item">
+                                <span class="icon"><i class="fa fa-fw m-r-10 fa-cog"></i></span>
+                                Manage
+                            </a>
+                            <hr class="navbar-divider">
+                            <a href="{{route('logout')}}" class="dropdown-item" onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                                <span class="icon"><i class="fa fa-fw m-r-10 fa-sign-out"></i></span>
+                                Logout
+                            </a>
+                            @include('_includes.forms.logout')
+                        </div>
+                    </div>
+                @endif
+            </div>
+        </div> 
+    </div>
+</nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,7 @@
 
     <title>DevMarketer</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
@@ -19,71 +18,26 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('styles')
+    {{-- This is so I can add CSS stylesheets to specific pages. 
+    So all the pages will have app.css but lets say i want to link a stylesheet for chart.js on my dashboard page 
+    to show charts on that page. On that one blade file I would add a @section('styles') and then put the stylesheet 
+    link in there and that will append it after my app.css file but only on that one page that needs that CSS sheet.
+    If my page doesn't need any custom css then i don't need to do anything and it will simply ignore it. So it gives 
+    me future flexibility. --}}
+
 </head>
 <body>
+    @include('_includes.nav.main')
     <div id="app">
-        <nav class="navbar has-shadow">
-            <div class="container">
-                <div class="navbar-brand" role="navigation" aria-label="main navigation">
-                    <a class="navbar-item" href="{{route('home')}}">
-                        <img src = "{{asset('images/devmarketer-logo.png')}}" alt="DevMarketer Logo" />
-                        {{-- From the asset helper, we can reference anything in the public folder...we can just do the url from the public folder. --}}
-                    </a>
-
-                    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                    </a>
-                </div>
-
-                <div class="navbar-menu" role="navigation" aria-label="main navigation">
-                    <div class="navbar-start">
-                        <a href="#" class="navbar-item is-tab is-hidden-mobile m-l-10">Learn</a>
-                        <a href="#" class="navbar-item is-tab is-hidden-mobile">Discuss</a>
-                        <a href="#" class="navbar-item is-tab is-hidden-mobile">Share</a>
-                    </div>
-
-                    <div class="navbar-end">
-                        @if (Auth::guest())
-                            <a href="{{route('login')}}" class="navbar-item is-tab">Login</a>
-                            <a href="{{route('register')}}" class="navbar-item is-tab">Join the Community</a>
-                        @else
-                            <div class="navbar-item has-dropdown is-hoverable">
-                                <a class="navbar-item is-tab">
-                                    <span>Hey Alex</span>
-                                    <span class="icon"><i class="fa fa-caret-down"></i></span>
-                                </a>
-                            
-                                <div class="navbar-dropdown is-right">
-                                    <a href="#" class="navbar-item">
-                                        <span class="icon"><i class="fa fa-fw m-r-10 fa-user-circle-o"></i></span>
-                                        Profile
-                                    </a>
-                                    <a href="#" class="navbar-item">
-                                        <span class="icon"><i class="fa fa-fw m-r-10 fa-bell"></i></span>
-                                        Notifications
-                                    </a>
-                                    <a href="#" class="navbar-item">
-                                        <span class="icon"><i class="fa fa-fw m-r-10 fa-cog"></i></span>
-                                        Settings
-                                    </a>
-                                    <hr class="navbar-divider">
-                                    <a href="#" class="dropdown-item">
-                                        <span class="icon"><i class="fa fa-fw m-r-10 fa-sign-out"></i></span>
-                                        Logout
-                                    </a>
-                                </div>
-                            </div>
-                        @endif
-                    </div>
-                </div> 
-            </div>
-        </nav>
-
         <main class="py-4">
             @yield('content')
         </main>
     </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    @yield('scripts')
+
 </body>
 </html>
